1e1f6f791bc978fafc27ca6c94bbf8fea8219979
Diego Marcher <diego@marcher.com.uy>
2020-02-06 18:41:34 -0300

    module selector completed

--- src/com/nerdscorner/android/plugin/android/dialog/ModuleSelectorForm.java
+++ src/com/nerdscorner/android/plugin/android/dialog/ModuleSelectorForm.java
@@ -3,13 +3,7 @@ package com.nerdscorner.android.plugin.android.dialog;
 import java.awt.event.KeyEvent;
 import java.io.IOException;
 
-import javax.swing.DefaultListModel;
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JDialog;
-import javax.swing.JList;
-import javax.swing.JPanel;
-import javax.swing.KeyStroke;
+import javax.swing.*;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ProjectRootManager;
@@ -18,16 +12,19 @@ import com.nerdscorner.android.plugin.android.commands.CommandExecutor;
 import com.nerdscorner.android.plugin.utils.FileCreator;
 
 public class ModuleSelectorForm extends JDialog {
-    private JList<VirtualFile> modulesList;
+    private JList<String> modulesList;
     private JButton acceptButton;
     private JButton cancelButton;
-    private JPanel contentPane;
+    private JPanel panel1;
+    private JList<String> targetBranchList;
+    private JList<String> sourceBranchList;
+    private JLabel resultText;
     private Project project;
 
     public ModuleSelectorForm(Project project) {
         this.project = project;
 
-        setContentPane(contentPane);
+        setContentPane(panel1);
         setModal(true);
         getRootPane().setDefaultButton(acceptButton);
 
@@ -36,29 +33,79 @@ public class ModuleSelectorForm extends JDialog {
         cancelButton.addActionListener(e -> onCancel());
 
         // call onCancel() on ESCAPE
-        contentPane.registerKeyboardAction(e -> onCancel(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        panel1.registerKeyboardAction(e -> onCancel(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
         loadProjectModules();
+        loadProjectBranches();
     }
 
     private void loadProjectModules() {
-        DefaultListModel<VirtualFile> model = new DefaultListModel<>();
+        DefaultListModel<String> model = new DefaultListModel<>();
         VirtualFile[] sourceFolders = ProjectRootManager.getInstance(project).getContentRoots();
-        for (VirtualFile sourceFolder : sourceFolders) {
-            model.addElement(sourceFolder);
+        for (int i = 1; i < sourceFolders.length; i++) {
+            model.addElement(sourceFolders[i].getName());
         }
-        this.modulesList.setModel(model);
+        modulesList.setModel(model);
+    }
+
+    private void loadProjectBranches() {
+        DefaultListModel<String> sourceBranchModel = new DefaultListModel<>();
+        DefaultListModel<String> targetBranchModel = new DefaultListModel<>();
+        CommandExecutor commandExecutor = new CommandExecutor(project.getBasePath());
+        String gitBranchResponse = commandExecutor.execute("git branch");
+        String[] branches = gitBranchResponse.split("\n");
+        for (String branch : branches) {
+            String branchName = branch.replaceAll("\\*", "").trim();
+            sourceBranchModel.addElement(branchName);
+            targetBranchModel.addElement(branchName);
+        }
+        sourceBranchList.setModel(sourceBranchModel);
+        targetBranchList.setModel(targetBranchModel);
     }
 
     private void onOK() {
-        try {
-            CommandExecutor commandExecutor = new CommandExecutor(project.getBasePath());
-            String moduleName = "app";
-            //Run dependencies for current branch
-            String result = commandExecutor.execute("./gradlew " + moduleName + ":dependencies -q");
-            FileCreator.INSTANCE.createFile(project.getBasePath(), "branch_dependencies.txt", result);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        new Thread(() -> {
+            SwingUtilities.invokeLater(() -> resultText.setText("Starting..."));
+            try {
+                String basePath = project.getBasePath();
+                CommandExecutor commandExecutor = new CommandExecutor(basePath);
+                String moduleName = modulesList.getSelectedValue();
+                // Save original branch
+                SwingUtilities.invokeLater(() -> resultText.setText("Saving original branch..."));
+                String gitStatusResult = commandExecutor.execute("git status");
+                String originalBranch = gitStatusResult
+                        .split("\n")[0]
+                        .split(" ")[2]; //format is: `On branch <branch_name>`
+
+                // Switch to source branch
+                SwingUtilities.invokeLater(() -> resultText.setText("Switching to source branch..."));
+                String sourceBranch = sourceBranchList.getSelectedValue();
+                commandExecutor.execute("git checkout " + sourceBranch);
+
+                //Run dependencies for source branch
+                SwingUtilities.invokeLater(() -> resultText.setText("Running dependencies for source branch..."));
+                String sourceBranchDependenciesResult = commandExecutor.execute("./gradlew " + moduleName + ":dependencies -q");
+                FileCreator.INSTANCE.createFile(basePath, sourceBranch + "_dependencies.txt", sourceBranchDependenciesResult);
+
+                // Switch to target branch
+                SwingUtilities.invokeLater(() -> resultText.setText("Switching to target branch..."));
+                String targetBranch = targetBranchList.getSelectedValue();
+                commandExecutor.execute("git checkout " + targetBranch);
+
+                //Run dependencies for target branch
+                SwingUtilities.invokeLater(() -> resultText.setText("Running dependencies for target branch..."));
+                String targetBranchDependenciesResult = commandExecutor.execute("./gradlew " + moduleName + ":dependencies -q");
+                FileCreator.INSTANCE.createFile(basePath, targetBranch + "_dependencies.txt", targetBranchDependenciesResult);
+
+                // Switch to original branch
+                SwingUtilities.invokeLater(() -> resultText.setText("Returning original branch..."));
+                commandExecutor.execute("git checkout " + originalBranch);
+
+                onCancel();
+            } catch (IOException e) {
+                e.printStackTrace();
+                SwingUtilities.invokeLater(() -> resultText.setText("FAILED! \n" + e.getMessage()));
+            }
+        }).start();
     }
 
     private void onCancel() {
